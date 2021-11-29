208815d08f9f41293548cb10086e9937ba304f21
Michael Walker <michaelwwalker42@gmail.com>
2021-11-28 19:03:07 -0700

    fixed typos in questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ var highScoresBtn = document.getElementById('viewHighScores');
 
 // retrieve high scores from local storage or create empty high scores array
 var highScores = [];
-if(localStorage.getItem("highScores") !== null) {
+if (localStorage.getItem("highScores") !== null) {
     highScores = JSON.parse(localStorage.getItem("highScores"));
 }
 
@@ -29,17 +29,17 @@ var questionArray = [
     {
         question: "The condition in an if/else statement is enclosed with________.",
         answer: "3. parenthesis",
-        options: ['1. quotes', '2 .curly brackets', '3 .parenthesis', '4. square brackets']
+        options: ['1. quotes', '2. curly brackets', '3. parenthesis', '4. square brackets']
     },
     {
         question: "Arrays in JavaScript can be used to store_________.",
         answer: "4. all of the above",
-        options: ['1. numbers and strings', '2. other arrays', '3. booleans', '4 .all of the above']
+        options: ['1. numbers and strings', '2. other arrays', '3. booleans', '4. all of the above']
     },
     {
         question: "String values must be enclosed within__________.",
         answer: "3. quotes",
-        options: ['1. commas', '2. curly brackets', '3. quotes', '4 .parenthesis']
+        options: ['1. commas', '2. curly brackets', '3. quotes', '4. parenthesis']
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
@@ -82,18 +82,20 @@ function start() {
 
 //-------------------------------------------nextQuestion function--------------------------------------------
 function nextQuestion() {
+   
     // display the next question
     questionTitle.textContent = questionArray[questionNumber].question;
     // clear out the question options
     questionOptions.innerHTML = '';
     // loop through the question array
-    questionArray[questionNumber].options.forEach(function (choice, i) {
+    questionArray[questionNumber].options.forEach(function (choice,) {
         // create choice buttons
         var choiceBtn = document.createElement('button');
         // set button attributes
         choiceBtn.setAttribute('class', 'optBtn');
         choiceBtn.setAttribute('value', choice);
         choiceBtn.textContent = choice;
+      
         // add event listener and check if answer is right on button click
         choiceBtn.onclick = checkAnswer;
         // append the choice buttons to the questionOptions element
@@ -103,18 +105,22 @@ function nextQuestion() {
 //-----------------------------------------End nextQuestion function------------------------------------------
 
 //----------------------------------------checkAnswer function-------------------------------------------------
-function checkAnswer() {  
+function checkAnswer() {
+    console.log(this.value + " was clicked");
+    console.log(questionArray[questionNumber].answer + " is the answer");
 
     // if the answer is wrong subtract 10 sectonds from time left
     if (questionArray[questionNumber].answer !== this.value) {
-        console.log(this);
         timeLeft -= 10;
         timer.textContent = timeLeft;
         showCorrectOrWrong.textContent = "Wrong!";
     } else {
         showCorrectOrWrong.textContent = "Correct!";
     }
-    questionNumber++;
+    if (questionNumber < questionArray.length) {
+        questionNumber++;
+    }
+
     // if there are no more questions end the game
     if (questionNumber === questionArray.length) {
         endGame()
@@ -140,7 +146,7 @@ function saveScore() {
     //push saved score into high scores array
     highScores.push(yourScore);
     // sorts array highest to lowest
-    highScores = highScores.sort((a,b) => {
+    highScores = highScores.sort((a, b) => {
         if (a.score < b.score) {
             return 1;
         } else {
@@ -150,9 +156,9 @@ function saveScore() {
     // keep high score list to 5 scores
     highScores.splice(5);
     // save high scores array to local storage
-    localStorage.setItem("highScores", JSON.stringify(highScores)); 
+    localStorage.setItem("highScores", JSON.stringify(highScores));
 
-        
+
 };
 //-----------------------------------------end saveScore function-----------------------------------------------
 
